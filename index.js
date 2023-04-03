440749e21c3a8641a1d844ded55be409c15c45f4
Ryan da Silva Oliveira <ryanprofissional02@gmail.com>
2023-04-03 11:12:37 -0300

    Configuração inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+// Importando bibliotecas
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+// configuração inicial
+const app = express();
+
+
+
+const PORT = 5000;
+app.listen(PORT, () => console.log('Server is running!'));
